23a7ef415b272188da9aa7ab81c431cc92cb1b8d
Vyacheslav <va.povhleb@gmail.com>
2024-08-20 17:47:13 +0300

    Implement the toReadable function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function toReadable (number) {
-  
+const units = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+const teens = ["ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"];
+const tens = ["", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"];
+const hundreds = "hundred";
+
+if (number < 10) {
+    return units[number];
+}
+
+if (number < 20) {
+    return teens[number - 10];
+}
+
+if (number < 100) {
+    return tens[Math.floor(number / 10)] + (number % 10 !== 0 ? " " + units[number % 10] : "");
+}
+
+if (number < 1000) {  return units[Math.floor(number / 100)] + " " + hundreds + (number % 100 !== 0 ? " " + toReadable(number % 100) : "");
+}
+
 }
